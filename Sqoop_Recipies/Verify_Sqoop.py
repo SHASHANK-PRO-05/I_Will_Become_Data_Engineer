5c5e680ed637ff682ec63ea735fbac93ace99e91
shanky <shashank.prof.05@gmail.com>
2018-07-18 00:00:33 -0700

    Working on Sqoop 2 recipes

--- Sqoop_Recipies/Verify_Sqoop.py
+++ Sqoop_Recipies/Verify_Sqoop.py
@@ -3,4 +3,11 @@ import subprocess
 sqoop2_command = ['sqoop2-tool']
 options = ['verify']
 
-print(subprocess.check_output(sqoop2_command + options))
+if "VerifyTool has finished correctly" in subprocess.check_output(sqoop2_command + options):
+    print("Your Sqoop 2 works perfectly")
+else:
+    print("Check your installation dude!!!!")
+    exit(1)
+    
+print('######################################')
+print('Starting your server and checking it ')
